3cfe8e6085be2fcae390888888fbcac434c36060
Ricardo Ribeiro <rmmro1@iscte-iul.pt>
2024-05-06 09:26:48 +0100

    Leitura de Ficheiros através de Url Sem erros

--- Projeto2023-2024-ES/src/EngSoftPackage/gui/LancaBrowser.java
+++ Projeto2023-2024-ES/src/EngSoftPackage/gui/LancaBrowser.java
@@ -6,25 +6,22 @@ import EngSoftPackage.html.CreateHTML;
 import EngSoftPackage.html.Horario;
 import java.awt.Desktop;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URISyntaxException;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.net.URL;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
 
 import javax.swing.JOptionPane;
 
 
 
+
 public class LancaBrowser  { 
+
 	
 	private final submitPage submitFilePage;
+	private static final String remoteFileLocalPath = "Projeto2023-2024-ES/assets/HorarioRemoto.csv";
 	
 	public LancaBrowser() {
 		submitFilePage = new submitPage();
@@ -61,13 +58,13 @@ public class LancaBrowser  {
 						 * para manipulaçãodos dados.
 						*/
 						URL remoteFile = new URL(filePath);
-						InputStream inputStream = remoteFile.openStream();
-						String remoteFileLocalPath = "Projeto2023-2024-ES/assets/HorarioRemoto.csv";
+						submitFilePage.setRemoteFile(true);
 						
 						//fazemos num if a criação do ficheiro local para sabermos se tivemos sucesso ou não na criação do mesmo
-						if (saveToLocalFile2(inputStream, remoteFileLocalPath)) {
+						if (saveToLocalFile(remoteFile, remoteFileLocalPath)) {
 
 							Horario horario = new Horario(remoteFileLocalPath);
+
 							CreateHTML ch = new CreateHTML(horario, remoteFileLocalPath);
 					    	ch.generateHTMLPage();
 					    	StringBuilder novaString = new StringBuilder();
@@ -81,16 +78,20 @@ public class LancaBrowser  {
 					    	    }
 					    	}
 					    	Desktop desk = Desktop.getDesktop();
-					    	desk.browse(new java.net.URI("file://" + novaString));
+					    	try {
+								System.out.println("file://" + System.getProperty("user.dir") + novaString);
+								desk.browse(new java.net.URI("file://" + System.getProperty("user.dir") + "/"+ novaString ));
+							} catch (IOException | URISyntaxException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+							submitFilePage.setVisible(false);
 						} else {
 							JOptionPane.showMessageDialog(submitFilePage, "Erro ao  Processar o ficheiro removo, tente novamente.",
 									"Error", JOptionPane.ERROR_MESSAGE);
 						}
 					} catch (IOException ex) {
 						throw new RuntimeException(ex);
-					} catch (URISyntaxException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
 					}
 				}
 				// Neste caso o input é um caminho para um ficheiro local
@@ -101,7 +102,6 @@ public class LancaBrowser  {
 						// Se existir usa o csv para gerar os dados para a pagina HTML e depois abre a
 						// pagina
 						Horario horario = new Horario(filePath);
-						System.out.println(horario);
 
 						/*Metodos de export abaixo, já funcionais mas comentados 
 						para ver aonde devem ser implementados */
@@ -158,9 +158,11 @@ public class LancaBrowser  {
 	 * @param inputStream   	faz a leitura do ficheiro. 
 	 * @param localFilePath 	localização local do ficheiro.
 	 */
-	private boolean saveToLocalFile(InputStream inputStream, String localFilePath) {
+	private boolean saveToLocalFile(URL remoteFile,  String localFilePath) {
+	
 		try {
 			File localFile = new File(localFilePath);
+			InputStream inputStream = remoteFile.openStream();
 
 			FileOutputStream outputStream = new FileOutputStream(localFile);
 			byte[] buffer = new byte[1024];
@@ -177,34 +179,6 @@ public class LancaBrowser  {
 			return false;
 		}
 	}
-
-	public boolean saveToLocalFile2(InputStream inputStream, String localFilePath) {
-        
-        try {
-            InputStreamReader reader = new InputStreamReader(inputStream);
-
-            // Criando um CSVParser para ler o arquivo CSV
-            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
-
-            // Criando um BufferedWriter para escrever os dados no novo arquivo CSV
-            BufferedWriter writer = new BufferedWriter(new FileWriter(localFilePath));
-
-            // Escrevendo os dados no novo arquivo CSV
-            for (CSVRecord record : csvParser) {
-                writer.write(String.join(",", record));
-                writer.newLine();
-            }
-
-            // Fechando os recursos
-            csvParser.close();
-            writer.close();
-			return true;
-
-        } catch (IOException e) {
-            e.printStackTrace();
-			return false;
-        }
-    }
 	
 }  
 
